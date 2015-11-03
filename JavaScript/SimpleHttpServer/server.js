e49aef4b687ded77ba1736bdf2c89d717c9b44af
LiaoYW <charlote@gmail.com>
2015-11-03 10:40:50 +0800

    create server for listening http request

---
+++ JavaScript/SimpleHttpServer/server.js
@@ -0,0 +1,14 @@
+//import http module 
+var http = require("http");
+
+//show "Hello World" at http://localhost:8888
+//create server for listening http request
+//use the nonsync_even_drive_callback_function
+http.createServer(function(request, response){
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World.");
+        response.end();
+        }).listen(8888);
+
+console.log("Server is started.");
